f501c6eb55f965bb900daa045fa6115e357ff580
asm-saim <saim777m@gmail.com>
2026-08-04 00:08:53 +0600

    delete , seal, freeze explained

--- keysValues.js
+++ keysValues.js
@@ -1,3 +1,4 @@
+//keys, values, entries:
 const person = {
   name: "Labib",
   age: 12,
@@ -11,3 +12,14 @@ for (let entry of entries) {
   [key, values] = entry;
   console.log(key, values);
 }
+
+//delete, seal, freeze:
+
+//delete:
+// delete person.name;
+
+//seal: delete, add not possible, edit possible
+Object.seal(person);
+
+//freeze: delete, add, edit not possible
+Object.freeze(person);
