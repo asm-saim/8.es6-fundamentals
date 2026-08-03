849f75efa807f5c63dbfbffe085ac0fad40eff8a
asm-saim <saim777m@gmail.com>
2026-08-03 11:44:06 +0600

    rest operator described

--- spread.js
+++ spread.js
@@ -1,13 +1,23 @@
-function spread(value) {
-  let newValue = [...value, 86, 789, 34];
-  return newValue;
-}
-console.log(spread([23, 56, 89, 23]));
+// function spread(value) {
+//   let newValue = [...value, 86, 789, 34];
+//   return newValue;
+// }
+// console.log(spread([23, 56, 89, 23]));
 
-//
-function studentPayment(name, amount) {
-  let info = ` Hello ${name}!
-Your paid amount is ${amount}`;
-  return info;
+// // find out the max us  ing max method.
+// const numbers = [23, 56, 34, 22, 455, 223];
+
+// //Logically , max only iterate on numbers, on array, it won't work. we need to use spread
+// const maxNum = Math.max(...numbers);
+// console.log(maxNum);
+
+//Rest operator:
+function restOp(a, b, ...restOperator) {
+  //   console.log(restOperator);
+  let sum = 0;
+  for (let i of restOperator) {
+    sum += i;
+  }
+  return a + b + sum;
 }
-console.log(studentPayment("Ekram", 4500));
+console.log(restOp(20, 30, 67, 4, 2, 566, 32, 11, 4, 55, 2));
--- template-string.js
+++ template-string.js
@@ -2,3 +2,11 @@ function template(value) {
   return `this is template ${value}`;
 }
 console.log(template("String"));
+
+//
+function studentPayment(name, amount) {
+  let info = ` Hello ${name}!
+Your paid amount is ${amount}`;
+  return info;
+}
+console.log(studentPayment("Ekram", 4500));
