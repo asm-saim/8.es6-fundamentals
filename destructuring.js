818bbbd08539f752a9ec69f91562159fa708a58d
asm-saim <saim777m@gmail.com>
2026-08-03 20:05:51 +0600

    object destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,4 +1,14 @@
 //array destructuring:
 const numbers = [34, 6, 22, 44, 72];
 const [first, second, , , last] = numbers;
-console.log(first,second,last)
+console.log(first, second, last);
+
+//object destructuring:
+const info = {
+  name: "jasim",
+  age: 23,
+  location: "USA",
+};
+
+const { name, age: boyosh, location } = info;
+console.log(name, boyosh, location);
